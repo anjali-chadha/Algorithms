496a80b619859958fe72356b925590d6c8cbe2c7
Anjali Chadha <6985729+anjali-chadha@users.noreply.github.com>
2018-11-01 23:13:01 -0500

    Create PalindromePair.java

---
+++ hackerrank/PalindromePair.java
@@ -0,0 +1,6 @@
+/*
+["gab", "cat", "bag", "alpha"] => [["gab", "bag"], ["bag", "gab"]]
+["gab", "cat", "bag", "alpha", "nurses", "race", "car", "run"] => [["gab", "bag"], ["bag", "gab"], ["race", "car"], ["nurses", "run"]]
+  ["cat", "dog", "elephant", "tac", "aire", "gigeria"] => [["cat", "tac"], ["tac", "cat"], ["aire", "gigeria"]]
+  ["cat", "dog", "elephant", "tac", "cath", "aire", "gigeria"] => [["cat", "tac"], ["tac", "cat"], ["cath", "tac"], ["aire", "gigeria"]]
+*/
