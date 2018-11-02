dc42939a398e0fcd9dd28bce602d9a002596d1c6
Anjali Chadha <6985729+anjali-chadha@users.noreply.github.com>
2018-11-01 23:13:56 -0500

    Create TravelBuddy.java

---
+++ hackerrank/TravelBuddy.java
@@ -0,0 +1,11 @@
+// Travel Buddies
+// A is TB of B if A shared 50% or more of WL with B
+// score is number of cities in common
+
+// John,Amsterdam,Barcelona,London,Prague
+// Yu,Shanghai,Hong Kong,Moscow,Sydney,Melbourne
+// Liz,London,Boston,Amsterdam,Madrid
+// Stan,Barcelona,Prague,London,Sydney,Moscow
+// Me,Amsterdam,Barcelona,London,Prague
+  
+// getRecommendations(int numRecommendations) -> recommendations are cities that are not in my WL but in TB WL  
