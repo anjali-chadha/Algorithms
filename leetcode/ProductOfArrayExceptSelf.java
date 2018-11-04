18d46471b53f696205f8aabd1ce3af397a40ac75
Anjali Chadha <6985729+anjali-chadha@users.noreply.github.com>
2018-11-04 09:58:05 -0600

    Update ProductOfArrayExceptSelf.java

--- leetcode/ProductOfArrayExceptSelf.java
+++ leetcode/ProductOfArrayExceptSelf.java
@@ -1,3 +1,19 @@
+/*
+https://leetcode.com/problems/product-of-array-except-self/description/
+
+Algorithm 1: Time and Space Complexity - O(N)
+Use separate arrays for storing left and right products
+Then in final step combine them
+
+Algorithm 2: Time = O(N), Space - O(1)
+No need to use extra memory - left and right arrays.
+Use two passes and store result in single array.
+
+Algorithm 3: Convert the product to addition by using logarithm and antilogarithm
+Time - O(N) Space - O(1)
+
+*/
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         if(nums == null || nums.length == 0) return null;
