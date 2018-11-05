9e6a2cf3f4835a25029897a277b4f6c8b47748e4
Anjali Chadha <6985729+anjali-chadha@users.noreply.github.com>
2018-11-04 19:55:38 -0600

    Update AddTwoNumbers2.java

--- leetcode/AddTwoNumbers2.java
+++ leetcode/AddTwoNumbers2.java
@@ -1,3 +1,10 @@
+/*
+Solution 1: Using 2 stacks
+
+Solution 2: Calculate the difference betweeen the lists lengths
+Use recursion to solve from the right to left
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -38,5 +45,79 @@ class Solution {
             sum /= 10;
         }
         return result.val == 0? result.next : result;        
+    }
+}
+
+
+class Solution2 {
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        int size1 = getLength(l1);
+        int size2 = getLength(l2);
+        ListNode result = new ListNode(1);
+        int[] carry = new int[1];
+        
+        if(size1 == size2) addSameSize(l1, l2, result, carry);
+        else {
+            //List1 should be the bigger one
+            if(size1 < size2) {
+                ListNode temp = l1;
+                l1 = l2;
+                l2 = temp;
+            }
+            
+            int diff = Math.abs(size1- size2);
+            
+            ListNode temp = l1;
+            while(diff > 0) {
+                diff--;
+                temp = temp.next;    
+            }
+            
+            addSameSize(temp, l2, result, carry);
+           
+            diff = Math.abs(size1- size2);
+            propagateCarry(l1, result, carry, diff);
+        }
+        if(carry[0] > 0) {
+            return result;
+        }
+        return result.next;
+    }
+ 
+    private void propagateCarry(ListNode l1, ListNode result, int[] carry, int diff) {
+        if(diff == 0) return;
+        propagateCarry(l1.next, result, carry, diff-1);
+        int sum = l1.val + carry[0];
+        carry[0] = sum/10;
+        sum %= 10;
+        ListNode newNode = new ListNode(sum);
+        ListNode temp = result;
+        newNode.next = temp.next;
+        result.next = newNode;
+    }
+    
+    private void addSameSize(ListNode l1, ListNode l2, ListNode result, int[] carry) {
+        if(l1 == null) return;
+        addSameSize(l1.next, l2.next, result, carry);
+        int sum = l1.val + l2.val + carry[0];
+        carry[0] = sum/10;
+        sum = sum%10;
+        ListNode newNode = new ListNode(sum);
+        ListNode temp = result;
+        newNode.next = temp.next;
+        result.next = newNode;
+    }
+    
+    private int getLength(ListNode node) {
+        int size = 0;
+        
+        while(node != null) {
+            size++; node = node.next;
+        }
+        return size;
     }
 }
