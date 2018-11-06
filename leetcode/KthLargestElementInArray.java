60bc1f954549af82cd9fae0d92b092681807b000
Anjali Chadha <6985729+anjali-chadha@users.noreply.github.com>
2018-11-06 01:28:32 -0600

    Update KthLargestElementInArray.java

--- leetcode/KthLargestElementInArray.java
+++ leetcode/KthLargestElementInArray.java
@@ -1,5 +1,20 @@
-//https://leetcode.com/problems/kth-largest-element-in-an-array/
+/* 
+https://leetcode.com/problems/kth-largest-element-in-an-array/
 
+Alogrithm 1: 
+Sort the input array. Access the element by index K
+Time complexity - O(NlogN)
+Space Complexity - O(1)
+
+Algorithm 2: (Implemented Below)
+Using Priority Queue (Java) minHeap
+Time Complexity - O(N logK)
+Space Complexity - O(K)
+
+Algorithm 3: 
+Quick Select (similar to Quick Sort - stop when pivot element's index is equal to K)
+Worst case - O(N^2), Average Case - O(N)
+*/
 class Solution {
     //Priority Queue
     public int findKthLargest1(int[] nums, int k) {
