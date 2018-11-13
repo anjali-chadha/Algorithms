93dbe8b5dc1b18ff0b9040fae1207fc1ae0efd8d
Anjali Chadha <6985729+anjali-chadha@users.noreply.github.com>
2018-11-12 18:24:06 -0600

    Create ConvertSortedListBST.java

---
+++ leetcode/ConvertSortedListBST.java
@@ -0,0 +1,55 @@
+/*
+Approach 1: Bottom Up recursion
+Using a global variable to track the current node in linked list
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    private ListNode nodeList;
+    
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        int len = findLength(head);
+        nodeList = head;
+        return bottomUpBst(0, len - 1);
+    }
+    
+    private TreeNode bottomUpBst(int start, int end) {
+        if(start > end) return null;
+        int mid = start + (end - start)/2;
+        TreeNode left = bottomUpBst(start, mid - 1);
+        TreeNode node = new TreeNode(nodeList.val);
+        node.left = left;
+        nodeList = nodeList.next;
+        node.right = bottomUpBst(mid + 1, end);
+        return node;
+    }
+    
+    private int findLength(ListNode node) {
+        ListNode curr = node;
+        int val = 0;
+        while(curr != null) {
+            curr = curr.next;
+            val++;
+        }
+        return val;
+    }
+}
